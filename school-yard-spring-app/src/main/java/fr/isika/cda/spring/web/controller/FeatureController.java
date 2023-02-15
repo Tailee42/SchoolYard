2601a178171f3640193b033798ca67c416d74a2c
Marie <brierremarie@gmail.com>
2023-02-15 15:40:48 +0100

    commit for Mohamed

--- school-yard-spring-app/src/main/java/fr/isika/cda/spring/web/controller/FeatureController.java
+++ school-yard-spring-app/src/main/java/fr/isika/cda/spring/web/controller/FeatureController.java
@@ -1,22 +1,14 @@
 package fr.isika.cda.spring.web.controller;
 
 import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.support.RequestContextUtils;
 
 import fr.isika.cda.entities.Feature;
 import fr.isika.cda.spring.business.service.FeatureService;
@@ -53,5 +45,13 @@ public class FeatureController {
 		featureService.deleteFeature(feature);
 		return new ModelAndView("redirect:/featuresList");
 	}
+	
+	@GetMapping("/updateFeatureForm")
+	@ResponseBody
+	public ModelAndView updateFeature(@RequestParam Feature feature) {
+
+		return new ModelAndView("redirect:/featuresList");
+	}
+	
 
 }
