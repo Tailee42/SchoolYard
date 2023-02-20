a56f99350ae346ca17888926e4af3f21f7b73dfa
JosephIsika <josepbarreau85@gmail.com>
2023-02-20 17:04:51 +0100

    cards on userDashboard with memberRole

--- school-yard-jee-app/src/main/java/fr/isika/cda/beans/MembersListBean.java
+++ school-yard-jee-app/src/main/java/fr/isika/cda/beans/MembersListBean.java
@@ -6,7 +6,10 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.inject.Inject;
 
+import fr.isika.cda.entities.school.Admin;
 import fr.isika.cda.entities.school.Member;
+import fr.isika.cda.entities.student.Student;
+import fr.isika.cda.entities.teacher.Teacher;
 import fr.isika.cda.entities.users.User;
 import fr.isika.cda.repositories.MemberRepository;
 import fr.isika.cda.utils.SessionUtils;
@@ -18,25 +21,45 @@ public class MembersListBean {
 	private MemberRepository memberRepository;
 
 	private List<Member> members;
+	private String memberRole;
 
 	public List<Member> allMembersForOneUser() {
 		members = memberRepository.getAllMembersForOneUser(SessionUtils.getConnectedUser().getId());
 		return members;
 	}
-	
+
 	public String goToIndexSchool(Member member) {
 		SessionUtils.setConnectedMember(member);
 		SessionUtils.setCurrentSchool(member.getSchool());
 		return "indexSchool";
 	}
-	
+
+	public void getMemberRole(Member member) {
+		if (member instanceof Admin) {
+			memberRole = "Administrateur";
+		} else if (member instanceof Teacher) {
+			memberRole = "Professeur";
+		} else if (member instanceof Student) {
+			memberRole = "Elève";
+		} else {
+			memberRole = "Utilisateur sans aucun rôle";
+		}
+	}
+
 	public List<Member> getMembers() {
 		return members;
 	}
-	
+
 	public void setMembers(List<Member> members) {
 		this.members = members;
 	}
-	
-	
+
+	public String getMemberRole() {
+		return memberRole;
+	}
+
+	public void setMemberRole(String memberRole) {
+		this.memberRole = memberRole;
+	}
+
 }
