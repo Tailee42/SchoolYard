9a9e9d0d3ac66157c16ec8ba8493f3098fbcd4c3
JosephIsika <josepbarreau85@gmail.com>
2023-02-23 16:51:15 +0100

    link to showUnits for one school

--- school-yard-jee-app/src/main/java/fr/isika/cda/beans/ShowUnitsBean.java
+++ school-yard-jee-app/src/main/java/fr/isika/cda/beans/ShowUnitsBean.java
@@ -10,19 +10,25 @@ import javax.inject.Named;
 
 import fr.isika.cda.entities.lesson.Unit;
 import fr.isika.cda.repositories.UnitRepository;
+import fr.isika.cda.utils.SessionUtils;
 
 @ManagedBean
 public class ShowUnitsBean {
 
 	private Unit unit = new Unit();
 	private List<Unit> units = new ArrayList<>();
-
+	private Long schoolId = SessionUtils.getCurrentSchool().getId();
 
 	@Inject
 	private UnitRepository unitRepository;
 
 	public List<Unit> allUnits() {
 		units = unitRepository.getAll();
+		return units;
+	}
+
+	public List<Unit> allUnitsBySchool() {
+		units = unitRepository.getAllUnitsValidatedBySchool(schoolId);
 		return units;
 	}
 
@@ -41,5 +47,5 @@ public class ShowUnitsBean {
 	public void setUnits(List<Unit> units) {
 		this.units = units;
 	}
-	
+
 }
